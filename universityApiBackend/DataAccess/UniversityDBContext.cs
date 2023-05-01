9fd84db5d3cee3a9c15716f6a6faede9294ce697
Luis Fernando Ttito <luis.ttito@ucsp.edu.pe>
2023-05-01 18:49:21 -0500

    Add project files.

---
+++ universityApiBackend/DataAccess/UniversityDBContext.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.EntityFrameworkCore;
+using universityApiBackend.Models.DataModels;
+
+namespace universityApiBackend.DataAccess
+{
+    public class UniversityDBContext : DbContext
+    {
+        public UniversityDBContext(DbContextOptions<UniversityDBContext> options) : base(options) { }
+    
+        //Add DbSet (Tables of our Data Base)
+        public DbSet<User>? Users { get; set; }
+        public DbSet<Curso>? Cursos { get; set; }
+    }
+}
